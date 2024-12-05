919e6ddeefd596fed001c5ecbc8d6bf29011c2bb
Jhonatan de Lima Alves dos Santos <93107234+Jhow632@users.noreply.github.com>
2024-12-05 16:31:54 -0300

    Trabalho Foodzone

--- Cliente.java
+++ Cliente.java
@@ -1,40 +1,62 @@
-public class Cliente {
+import java.util.List;
+
+public final class Cliente
+{   private int  id;
     private String nome;
-    private String fone;
-    private int id;
-
-    public Cliente(int id, String nome, String fone) {
-        this.id = id;
+    private String  telefone;
+    private String  endereco;
+    private List<Pedido> pedidos;
+    
+    public  Cliente()
+    {   clearObject();
+    }
+    public  Cliente(int id, String nome, String telefone, String endereco)
+    {  
         this.nome = nome;
-        this.fone = fone;
+        this.telefone = telefone;
+        this.endereco = endereco;
     }
-
-    public int getId() {
-        return id;
+    
+    public  int  getId(){
+        return  id;
     }
-
-    public void setId(int id) {
+    public  void setId(int id){
         this.id = id;
+    }  
+    
+    public  String getNome()
+    {   return  nome;
     }
 
-    public String getNome() {
-        return nome;
+    public  void setNome(String nome)
+    {   this.nome = nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public  String getTelefone()
+    {   return  telefone;
     }
 
-    public String getFone() {
-        return fone;
+    public  void setTelefone(String telefone)
+    {   this.telefone = telefone;
     }
 
-    public void setFone(String fone) {
-        this.fone = fone;
+    public  String getEndereco()
+    {   return  endereco;
     }
 
-    @Override
-    public String toString() {
-        return "ID: " + id + ", Nome: " + nome + ", Fone: " + fone;
+    public  void setEndereco(String endereco)
+    {   this.endereco = endereco;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+    
+    public  void  clearObject()
+    {   nome = "Não há usuário cadastrado";
     }
 }
